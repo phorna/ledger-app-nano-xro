e6b5bac808786bdb3b34d99ac64a2a82b1f38a17
phorna <153530899+phorna@users.noreply.github.com>
2023-12-16 23:39:37 +0100

    Update coins.c

--- src/coins.c
+++ src/coins.c
@@ -37,6 +37,39 @@ REGISTER_COINS(
     })
     #endif
 
+    #if defined(HAVE_COIN_RAIBLOCKSONE)
+    COIN(LIBN_COIN_TYPE_RAIBLOCKSONE, {
+        .coinName = "RaiblocksOne",
+        #if defined(TARGET_BLUE)
+        .coinBadge = &C_blue_badge_raiblocksone,
+        #else
+        .coinBadge = &C_nanos_badge_raiblocksone,
+        #endif
+        .bip32Prefix = { HARDENED(44), HARDENED(165) },
+        .addressPrimaryPrefix = "xro_",
+        .addressSecondaryPrefix = "xro_",
+        .addressDefaultPrefix = LIBN_PRIMARY_PREFIX,
+        .defaultUnit = "RAIBLOCKSONE",
+        .defaultUnitScale = 30, // 1 RAIBLOCKSONE = 10^30 raw
+        #if defined(TARGET_BLUE)
+        .colorBackground = 0xF9F9F9,
+        .colorForeground = 0x000000,
+        .colorAltBackground = 0x4A90E2,
+        .colorAltForeground = 0xE0FFFF,
+        .colorRejectBackground = 0xC6C6C6,
+        .colorRejectForeground = 0x000000,
+        .colorRejectOverBackground = 0xADADAD,
+        .colorRejectOverForeground = 0x000000,
+        .colorConfirmBackground = 0x4A90E2,
+        .colorConfirmForeground = 0xE0FFFF,
+        .colorConfirmOverBackground = 0x3177C9,
+        .colorConfirmOverForeground = 0xFFFFFF,
+        .iconToggleOff = &C_blue_icon_toggle_off,
+        .iconToggleOn = &C_blue_icon_toggle_on_raiblocksone,
+        #endif // TARGET_BLUE
+    })
+    #endif
+
     #if defined(HAVE_COIN_BANANO)
     COIN(LIBN_COIN_TYPE_BANANO, {
         .coinName = "Banano",
